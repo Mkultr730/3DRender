4178f8fb873f8c1f01272d594ea893263d43b6f5
Martin Molinares <memolinares@uninorte.edu.co>
2019-10-11 14:08:24 -0500

    Rotation added

--- src/pkg3drender/Viewer.java
+++ src/pkg3drender/Viewer.java
@@ -9,6 +9,7 @@ import java.awt.*;
 import java.awt.geom.Path2D;
 import java.util.ArrayList;
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
 
 /**
  *
@@ -46,21 +47,47 @@ public class Viewer {
                 tris.add(new Triangle(new Vertex(-100, 100, -100), new Vertex(100, -100, -100), new Vertex(100, 100, 100), Color.GREEN));
                 tris.add(new Triangle(new Vertex(-100, 100, -100), new Vertex(100, -100, -100), new Vertex(-100, -100, 100), Color.BLUE));
                 
+                double heading = Math.toRadians(headSlider.getValue());
+                Matrix3 headingTransform = new Matrix3(new double[]{
+                        Math.cos(heading), 0, -Math.sin(heading), 
+                        0,1,0,
+                        Math.sin(heading), 0, Math.cos(heading)
+                });
+                
+                double pitch = Math.toRadians(pitchSlider.getValue());
+                Matrix3 pitchTransform = new Matrix3(new double[]{
+                    1, 0, 0,
+                    0, Math.cos(pitch), Math.sin(pitch),
+                    0, -Math.sin(pitch), Math.cos(pitch)
+                });
+                Matrix3 transform = headingTransform.multiply(pitchTransform);
+                
                 
                 g2.translate(getWidth()/2, getHeight()/2);
                 g2.setColor(Color.WHITE);
                 for (Triangle t : tris) {
+                    Vertex v1 = transform.transform(t.v1);
+                    Vertex v2 = transform.transform(t.v2);
+                    Vertex v3 = transform.transform(t.v3);
                     Path2D path = new Path2D.Double();
-                    path.moveTo(t.v1.x, t.v1.y);
-                    path.lineTo(t.v2.x, t.v2.y);
-                    path.lineTo(t.v3.x, t.v3.y);
+                    path.moveTo(v1.x, v1.y);
+                    path.lineTo(v2.x, v2.y);
+                    path.lineTo(v3.x, v3.y);
                     path.closePath();
                     g2.draw(path);
                 }
-            };
+                
+                
+                
+                
+                
+            }
         };
         
         pane.add(renderPanel, BorderLayout.CENTER);
+        
+        headSlider.addChangeListener((ChangeEvent e) -> renderPanel.repaint());
+        pitchSlider.addChangeListener((ChangeEvent e) -> renderPanel.repaint());
         
         frame.setSize(400, 400);
         frame.setVisible(true);
